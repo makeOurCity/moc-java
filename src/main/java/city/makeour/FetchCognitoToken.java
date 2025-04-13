6e4e8cf4d07cfee81c266f1714cfadbc2f119c8a
Shugo USHIO <ushio.s@gmail.com>
2025-04-13 21:58:20 +0900

    build succeeded

--- src/main/java/city/makeour/FetchCognitoToken.java
+++ src/main/java/city/makeour/FetchCognitoToken.java
@@ -3,79 +3,69 @@ package city.makeour;
 import java.util.HashMap;
 import java.util.Map;
 
+import software.amazon.awssdk.services.cognitoidentityprovider.CognitoIdentityProviderClient;
 import software.amazon.awssdk.services.cognitoidentityprovider.model.AdminInitiateAuthRequest;
+import software.amazon.awssdk.services.cognitoidentityprovider.model.AdminInitiateAuthResponse;
 import software.amazon.awssdk.services.cognitoidentityprovider.model.AuthFlowType;
+import software.amazon.awssdk.services.cognitoidentityprovider.model.AuthenticationResultType;
 
 public class FetchCognitoToken {
 
-    protected CognitoClient client;
+    protected CognitoIdentityProviderClient client;
     protected String cognitoUserPoolId;
     protected String cognitoClientId;
     protected String refreshToken;
+
+    protected String username;
+    protected String password;
 
     public void construct(String cognitoUserPoolId, String cognitoClientId) {
         this.cognitoClientId = cognitoClientId;
         this.cognitoUserPoolId = cognitoUserPoolId;
         this.refreshToken = null;
-        this.client = CognitoClient.getInstance();
+        // this.client = CognitoIdentityProviderClient.getInstance();
     }
 
-    public String fetchToken(String username, String password) {
-        Map<String, String> authParameters = new HashMap<>();
-        authParameters.put("USERNAME", username);
-        authParameters.put("PASSWORD", password);
+    public void setAuthParameters(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
 
-        AdminInitiateAuthRequest request = new AdminInitiateAuthRequest();
-        request
-                .withAuthFlow(AuthFlowType.ADMIN_NO_SRP_AUTH)
-                .withUserPoolId(this.cognitoUserPoolId)
-                .withClientId(this.cognitoClientId)
-                .withAuthParameters(authParameters);
+    public String fetchToken() {
+        AdminInitiateAuthRequest authRequest = null;
 
-        String idToken = null;
-        AdminInitiateAuthResult response = client.adminInitiateAuth(request);
-        response.getAuthenticationResult().ifPresent(authResult -> {
-            this.refreshToken = authResult.getRefreshToken();
-            idToken = authResult.getIdToken();
-            System.out.println("Access Token: " + authResult.getAccessToken());
-            System.out.println("ID Token: " + authResult.getIdToken());
-            System.out.println("Refresh Token: " + this.refreshToken);
-        });
+        if (this.refreshToken != null) {
+            Map<String, String> authParameters = new HashMap<>();
+            authParameters.put("REFRESH_TOKEN", this.refreshToken);
+
+            authRequest = AdminInitiateAuthRequest.builder()
+                    .clientId(this.cognitoClientId)
+                    .userPoolId(this.cognitoUserPoolId)
+                    .authParameters(authParameters)
+                    .authFlow(AuthFlowType.REFRESH_TOKEN_AUTH)
+                    .build();
+        } else {
+            Map<String, String> authParameters = new HashMap<>();
+            authParameters.put("USERNAME", username);
+            authParameters.put("PASSWORD", password);
+
+            authRequest = AdminInitiateAuthRequest.builder()
+                    .clientId(this.cognitoClientId)
+                    .userPoolId(this.cognitoUserPoolId)
+                    .authParameters(authParameters)
+                    .authFlow(AuthFlowType.ADMIN_USER_PASSWORD_AUTH)
+                    .build();
+        }
+
+        AdminInitiateAuthResponse response = this.client.adminInitiateAuth(authRequest);
+        AuthenticationResultType result = response.authenticationResult();
+
+        this.refreshToken = result.refreshToken();
+
         if (this.refreshToken == null) {
             System.out.println("Failed to fetch token");
         }
 
-        return idToken;
-    }
-
-    public String refreshToken() {
-        if (this.refreshToken == null) {
-            System.out.println("Refresh token is null");
-            return null;
-        }
-
-        Map<String, String> authParameters = new HashMap<>();
-        authParameters.put("REFRESH_TOKEN", this.refreshToken);
-
-        AdminInitiateAuthRequest request = new AdminInitiateAuthRequest();
-        request
-                .withAuthFlow(AuthFlowType.REFRESH_TOKEN_AUTH)
-                .withUserPoolId(this.cognitoUserPoolId)
-                .withClientId(this.cognitoClientId)
-                .withAuthParameters(authParameters);
-
-        String idToken = null;
-        AdminInitiateAuthResult response = client.adminInitiateAuth(request);
-        response.getAuthenticationResult().ifPresent(authResult -> {
-            this.refreshToken = authResult.getRefreshToken();
-            idToken = authResult.getIdToken();
-            System.out.println("Access Token: " + authResult.getAccessToken());
-            System.out.println("ID Token: " + authResult.getIdToken());
-            System.out.println("Refresh Token: " + this.refreshToken);
-        });
-        if (this.refreshToken == null) {
-            System.out.println("Failed to refresh token");
-        }
-        return idToken;
+        return result.idToken();
     }
 }
