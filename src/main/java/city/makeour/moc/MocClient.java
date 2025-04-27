360e8ea009c0df10b26af6d31a8c3c42b3e099b7
Shugo USHIO <ushio.s@gmail.com>
2025-04-27 20:39:51 +0900

    add client

--- src/main/java/city/makeour/moc/MocClient.java
+++ src/main/java/city/makeour/moc/MocClient.java
@@ -1,99 +1,25 @@
 package city.makeour.moc;
 
-import java.util.List;
-
-import city.makeour.ngsi.v2.api.ApiEntryPointApi;
 import city.makeour.ngsi.v2.api.EntitiesApi;
 import city.makeour.ngsi.v2.invoker.ApiClient;
-import city.makeour.ngsi.v2.invoker.ApiException;
-import city.makeour.ngsi.v2.model.ListEntitiesResponse;
-import city.makeour.ngsi.v2.model.RetrieveApiResourcesResponse;
 
 public class MocClient {
-    private String accessToken;
-    private String refreshToken;
-    private final ApiClient apiClient;
-    private final ApiEntryPointApi apiEntryPoint;
-    private final EntitiesApi entitiesApi;
+    protected ApiClient apiClient;
 
-    public MocClient() throws ApiException {
-        this("https://orion.sandbox.makeour.city/v2");
+    protected EntitiesApi entitiesApi;
+
+    public MocClient() {
+        this("https://orion.sandbox.makeour.city");
     }
 
-    public MocClient(String baseUri) throws ApiException {
+    public MocClient(String basePath) {
         this.apiClient = new ApiClient();
-        this.apiClient.updateBaseUri(baseUri);
-        this.apiEntryPoint = new ApiEntryPointApi(apiClient);
-        this.entitiesApi = new EntitiesApi(apiClient);
-        validateConnection();
+        this.apiClient.setBasePath(basePath);
+
+        this.entitiesApi = new EntitiesApi(this.apiClient);
     }
 
-    // テスト用のコンストラクタ
-    MocClient(String baseUrl, ApiClient apiClient, ApiEntryPointApi apiEntryPoint, EntitiesApi entitiesApi)
-            throws ApiException {
-        apiClient.updateBaseUri(baseUrl);
-        this.apiClient = apiClient;
-        this.apiEntryPoint = apiEntryPoint;
-        this.entitiesApi = entitiesApi;
-        validateConnection();
-    }
-
-    public void updateBaseUri(String baseUrl) {
-        apiClient.updateBaseUri(baseUrl);
-    }
-
-    private void validateConnection() throws ApiException {
-        RetrieveApiResourcesResponse result = apiEntryPoint.retrieveAPIResources();
-        if (result == null) {
-            throw new ApiException("Failed to retrieve API resources");
-        }
-    }
-
-    public void setAccessToken(String accessToken) {
-        this.accessToken = accessToken;
-    }
-
-    public void setRefreshToken(String refreshToken) {
-        this.refreshToken = refreshToken;
-    }
-
-    public String getAccessToken() {
-        return accessToken;
-    }
-
-    public String getRefreshToken() {
-        return refreshToken;
-    }
-
-    /**
-     * Fiware-Serviceヘッダの値を設定します
-     *
-     * @param fiwareService テナント名
-     */
-    public void setFiwareService(String fiwareService) {
-        throw new UnsupportedOperationException("Fiware-Service header is not supported");
-    }
-
-    /**
-     * エンティティの一覧を取得します
-     *
-     * @return エンティティのリスト
-     * @throws ApiException API呼び出しが失敗した場合
-     */
-    public List<ListEntitiesResponse> listEntities() throws ApiException {
-        return entitiesApi.listEntities(null, null, null, null, null, null, null, null,
-                null, null, null, null, null, null, null);
-    }
-
-    /**
-     * エンティティの一覧をページ指定で取得します
-     * 
-     * @param offset オフセット値
-     * @return エンティティのリスト
-     * @throws ApiException API呼び出しが失敗した場合
-     */
-    public List<ListEntitiesResponse> listEntities(Double offset) throws ApiException {
-        return entitiesApi.listEntities(null, null, null, null, null, null, null, null,
-                null, null, offset, null, null, null, null);
+    public EntitiesApi entities() {
+        return this.entitiesApi;
     }
 }
