3ebf5054f8554b89f1d5a5e4ac54e53f6a8e6c77
Shugo USHIO <ushio.s@gmail.com>
2025-04-27 21:10:39 +0900

    Add test

--- src/test/java/city/makeour/moc/MocClientTest.java
+++ src/test/java/city/makeour/moc/MocClientTest.java
@@ -3,10 +3,13 @@ package city.makeour.moc;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+import java.util.List;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import city.makeour.ngsi.v2.api.EntitiesApi;
+import city.makeour.ngsi.v2.model.ListEntitiesResponse;
 
 class MocClientTest {
 
@@ -33,5 +36,20 @@ class MocClientTest {
 
         assertNotNull(entitiesApi);
         assertEquals(client.entitiesApi, entitiesApi);
+    }
+
+    @Test
+    @DisplayName("entities apiで、entityの一覧を取得するテスト")
+    void testEntitiesApi() {
+        MocClient client = new MocClient();
+        EntitiesApi entitiesApi = client.entities();
+
+        List<ListEntitiesResponse> list = entitiesApi.listEntities(null, null, null, null, null, null, null, null, null,
+                null, null, null,
+                null,
+                null,
+                null);
+        assertNotNull(list);
+        System.out.println("Entities: " + list);
     }
 }
