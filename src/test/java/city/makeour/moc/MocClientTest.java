579be55830f6eb8c9de7ca04bdb5c5ca8ed9a494
mohirose <143729590+mohirose@users.noreply.github.com>
2025-10-01 11:19:39 +0900

    Update src/test/java/city/makeour/moc/MocClientTest.java
    Co-authored-by: Shugo USHIO <ushio.s@gmail.com>

--- src/test/java/city/makeour/moc/MocClientTest.java
+++ src/test/java/city/makeour/moc/MocClientTest.java
@@ -136,7 +136,7 @@ class MocClientTest {
         entity.setId(entityId);
 
         // 作成を実行
-        client.entities().createEntity("application/json", entity, null);
+        client.entities().createEntity("application/json", entity, "keyValues");
 
         // getEntityの呼び出し、レスポンスの変換
         RetrieveEntityResponse retrievedEntity = client
